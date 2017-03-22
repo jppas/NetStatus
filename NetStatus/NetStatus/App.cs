505edf7871a012215a2159f362d29217d229d97f
Peevee Jace G. Pascual <peevee12@yahoo.com>
2017-03-22 13:05:47 -0600

    Initial Check in

---
+++ NetStatus/NetStatus/App.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public class App:Application
+    {
+        public App()
+        {
+            MainPage = CrossConnectivity.Current.IsConnected ? (Page) new NetworkViewPage() : new NoNetworkPage();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+        }
+
+        private void HandleConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+
+            if(e.IsConnected&&currentPage!=typeof(NetworkViewPage))
+                this.MainPage= new NetworkViewPage();
+            else if(!e.IsConnected&& currentPage!=typeof(NoNetworkPage))
+                this.MainPage= new NoNetworkPage();
+        }
+
+        protected override void OnSleep()
+        {
+           
+        }
+
+        protected override void OnResume()
+        {
+             
+        }
+        
+    }
+}
---
+++ NetStatus/NetStatus/NetworkViewPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public class NetworkViewPage:ContentPage
+    {
+        private Label ConnectionDetails = new Label();
+        public NetworkViewPage()
+        {
+            this.Padding= new Thickness(20,20,20,20);
+
+            var panel = new StackLayout
+            {
+                Spacing=15
+            };
+
+           panel.Children.Add(ConnectionDetails);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+            if(CrossConnectivity.Current!=null)
+                CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+
+        private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
+            
+        }
+    }
+}
